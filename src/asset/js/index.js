3168f82ab9a5fbf28809586db26b6c15aa6b0d99
12Yu <12yudev@gmail.com>
2024-04-29 11:01:46 -0700

    progress bar

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -1,3 +1,4 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/textfield/filled-text-field.js';
 import '@material/web/dialog/dialog.js';
+import '@material/web/progress/circular-progress.js';
