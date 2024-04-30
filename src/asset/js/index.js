a9243665e449a48384442737db15e09e99afd8cc
12Yu <12yudev@gmail.com>
2024-04-29 17:26:04 -0700

    add tabs

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -2,3 +2,5 @@ import '@material/web/button/filled-button.js';
 import '@material/web/textfield/filled-text-field.js';
 import '@material/web/dialog/dialog.js';
 import '@material/web/progress/circular-progress.js';
+import '@material/web/tabs/tabs.js';
+import '@material/web/tabs/primary-tab.js';
