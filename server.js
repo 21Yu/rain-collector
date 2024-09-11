416e67671a4f3fbd5ee925cbb7b07bf697243260
12Yu <12yudev@gmail.com>
2024-09-10 23:14:18 -0700

    mongo db setup

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/raincollector')
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
