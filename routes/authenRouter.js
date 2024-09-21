30f9baef2d9a91309c61fb10423ae239484b6060
12Yu <12yudev@gmail.com>
2024-09-21 16:00:36 -0700

    dashboard intro

--- routes/authenRouter.js
+++ routes/authenRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const authUser = require('../models/authUser')
 
 router.get('/', (req, res) => {
-    res.render('authen/login')
+    res.render('authen/login', { message: ''})
 })
 
 router.get('/signUp', (req, res) => {
@@ -21,6 +21,25 @@ router.post('/', async (req, res) => {
         res.render('authen/login', { message: 'Sign up success'})
     } catch {
         res.render('authen/login', { message: 'error signing up'})
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await authUser.findOne({ userName: req.body.userName});
+
+        if (!user) {
+            return res.render('authen/login', { message: 'user does not exist'})
+        }
+
+        if (user.password == req.body.password) {
+            return res.render('userPages/dashboard', { userMessage: user.userName})
+        } else {
+            return res.render('authen/login', { message: 'wrong password'})
+        }
+        
+    } catch {
+        res.render('authen/login', { message: 'something went wrong'})
     }
 })
 module.exports = router
